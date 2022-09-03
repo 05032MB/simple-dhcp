43fa550038d75ce962368b8c5d2512c3af18c772
inf141243 <mateusz.karlic@student.put.poznan.pl>
2022-09-03 21:57:40 +0200

    Add option to bind to device

--- socket.hpp
+++ socket.hpp
@@ -24,15 +24,15 @@ public:
     udpsocketserver() {
         sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         if(sock < 0)
-            THROW_RUNTIME_GET_ERRNO("Cannot open socket: ");
+            THROW_RUNTIME_GET_ERRNO("Cannot open socket");
 
         int opt = 1;
         if(setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &opt, sizeof(opt)) < 0)
-            THROW_RUNTIME_GET_ERRNO("Cannot setopt brd for socket: ");
+            THROW_RUNTIME_GET_ERRNO("Cannot setopt brd for socket");
 
         opt = siz;
         if(setsockopt(sock, SOL_SOCKET, SO_RCVBUF, &opt, sizeof(opt)) < 0 )
-            THROW_RUNTIME_GET_ERRNO("Cannot setopt size for socket: ");
+            THROW_RUNTIME_GET_ERRNO("Cannot setopt size for socket");
 
         socklen_t st = 0;
         getsockopt(sock, SOL_SOCKET, SO_RCVBUF, &opt, &st);
@@ -48,15 +48,20 @@ public:
         
         int _addr = htonl(INADDR_ANY);
         if(!addr.empty()) {
-           _addr = inet_addr(addr.c_str());
-           if(_addr == -1) {
-               THROW_RUNTIME_GET_ERRNO("Cannot bind the socket to the address.");
-           }
+            _addr = inet_addr(addr.c_str());
+            if(_addr == -1) {
+                std::cout << "Cannot bind the socket to the address, trying as an interface: " << addr << std::endl;
+                // is iface name
+                // https://stackoverflow.com/questions/1207746/problems-with-so-bindtodevice-linux-socket-option
+                if(setsockopt(sock, SOL_SOCKET, SO_BINDTODEVICE, addr.c_str(), addr.size()) < 0) {
+                    THROW_RUNTIME_GET_ERRNO("Cannot bind the socket to the interface");
+                }
+            } else {
+                saddr.sin_addr.s_addr = _addr;
+            }
         }
-        saddr.sin_addr.s_addr = _addr;
-
         if(::bind(sock, reinterpret_cast<sockaddr*>(&saddr), sizeof(saddr)) < 0)
-            THROW_RUNTIME_GET_ERRNO("Cannot bind socket: ");
+            THROW_RUNTIME_GET_ERRNO("Cannot bind socket");
     }
 
     std::pair<const buffer&, const sockaddr_in&> receive() {
